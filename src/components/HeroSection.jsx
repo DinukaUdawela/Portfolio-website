516d91c14fcb3ede166ba0db6931fe1c81cd0cfe
DinukaUdawela <126551484+DinukaUdawela@users.noreply.github.com>
2024-07-19 19:50:51 +0530

    hero updates 2

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,13 +7,27 @@ const HeroSection = () => {
         <div className="grid grid-cols-1 lg:grid-cols-12">
             <div className="col-span-7 place-self-center">
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">Hello I'm Dinuka</h1>
-                <p className="text-[#ADB7BE] text-lg lg:text-xl">
+                <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi laboriosam a voluptatum eos rem, aliquid reprehenderit ipsam temporibus magni doloribus?</p>
             </div>  
-            <div className="col-span-5">
+                    <div>
+                        <div
+                        className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+                        >
+                        Hire Me
+                        </div>
+                        <div
+                        className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+                        >
+                        <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                            Download CV
+                        </span>
+                        </div>
+                    </div>
+            <div className="col-span-5 place self center mt-4 lg:mt-0">
                 <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                     <Image
-                        src="/images/hero-image.png"
+                        src="/images/hero-image.jpg"
                         alt="hero image"
                         className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                         width={300}
