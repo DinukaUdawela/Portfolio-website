95ad9091cdbe2967be014f2ed074bd9cb2eb6f89
DinukaUdawela <126551484+DinukaUdawela@users.noreply.github.com>
2024-07-23 16:34:06 +0530

    Update ProjectsSection.jsx

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -100,6 +100,11 @@ const ProjectsSection = () => {
           name="Mobile"
           isSelected={tag === "Mobile"}
         />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="IOT"
+          isSelected={tag === "IOT"}
+        />
       </div>
       <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
         {filteredProjects.map((project, index) => (
