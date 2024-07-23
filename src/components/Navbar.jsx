4a29f5234ad8e38b57d7cfbe3000ca2a490811e9
DinukaUdawela <126551484+DinukaUdawela@users.noreply.github.com>
2024-07-23 09:49:02 +0530

    Nave bar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 "use client";
 import Link from "next/link";
 import React, { useState } from "react";
-
+import NavLink from "./NavLink";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
   {
@@ -36,7 +38,7 @@ const Navbar = () => {
               onClick={() => setNavbarOpen(true)}
               className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
             >
-              {/* <Bars3Icon className="h-5 w-5" /> */}
+              <Bars3Icon className="h-5 w-5" />
             </button>
           ) : (
             <button
@@ -51,7 +53,7 @@ const Navbar = () => {
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
             {navLinks.map((link, index) => (
               <li key={index}>
-    
+                <NavLink href={link.path} title={link.title} />
               </li>
             ))}
           </ul>
