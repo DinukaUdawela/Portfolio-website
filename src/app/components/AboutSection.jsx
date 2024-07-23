9cca32bada9da2e4f5b2681091dadbd861175374
DinukaUdawela <126551484+DinukaUdawela@users.noreply.github.com>
2024-07-23 15:41:34 +0530

    Update AboutSection.jsx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -9,12 +9,13 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <ul className="list-disc pl-2">
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>PostgreSQL</li>
-        <li>Sequelize</li>
         <li>JavaScript</li>
         <li>React</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>C</li>
+        <li>Figma / adobeXD</li>
+
       </ul>
     ),
   },
@@ -23,8 +24,8 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Fullstack Academy of Code</li>
-        <li>University of California, Santa Cruz</li>
+        <li>BSc. Hons. In Computing & Information Systems (Reading) Sabaragamuwa University of Sri Lanka</li>
+        <li>Intel Easy Steps</li>
       </ul>
     ),
   },
@@ -33,8 +34,7 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>AWS Cloud Practitioner</li>
-        <li>Google Professional Cloud Developer</li>
+        <li>Intel Easy Steps</li>
       </ul>
     ),
   },
